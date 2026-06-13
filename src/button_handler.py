8d58d63fb03fad95607cff6dadea89216622207a
Simon Jäger <simonjager@hotmail.com>
2026-06-13 16:46:26 +0200

    setting up signals

--- src/button_handler.py
+++ src/button_handler.py
@@ -5,8 +5,11 @@ import sys
 
 from interface.ui_everyday import Ui_everydayProjectEditor
 from interface.ui_main import Ui_ProjectPlannerMain
+from interface.ui_new_project import Ui_addNewProject
 from interface.ui_programming import Ui_programmingProjectEditor
 from interface.ui_tabs import Ui_Viewer
+
+
 
 
 # Universal buttons
@@ -24,12 +27,22 @@ def exit_clicked():
 # Main menu buttons
 def add_project_clicked():
     print("Add project clicked!")
+Ui_ProjectPlannerMain.addProject.clicked.connect(add_project_clicked)
 
 def view_projects_clicked():
     print("View projects clicked")
+Ui_ProjectPlannerMain.viewProjects.clicked.connect(view_projects_clicked)
 
 def view_archive_clicked():
     print("View archive clicked")
+Ui_ProjectPlannerMain.viewArchive.clicked.connect(view_archive_clicked)
 
 # Add project menu buttons
-def 
+def everyday_project_clicked():
+    print("Add everyday project clicked")
+Ui_addNewProject.addEveryday.clicked.connect(everyday_project_clicked)
+
+def programming_project_clicked():
+    print("Add programming project clicked")
+Ui_addNewProject.addProgramming.clicked.connect(programming_project_clicked)
+
--- src/interface/ui_new_project.py
+++ src/interface/ui_new_project.py
@@ -18,12 +18,12 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
 from PySide6.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QPushButton,
     QSizePolicy, QVBoxLayout, QWidget)
 
-class Ui_Form(object):
-    def setupUi(self, Form):
-        if not Form.objectName():
-            Form.setObjectName(u"Form")
-        Form.resize(375, 290)
-        self.widget = QWidget(Form)
+class Ui_addNewProject(object):
+    def setupUi(self, addNewProject):
+        if not addNewProject.objectName():
+            addNewProject.setObjectName(u"addNewProject")
+        addNewProject.resize(375, 290)
+        self.widget = QWidget(addNewProject)
         self.widget.setObjectName(u"widget")
         self.widget.setGeometry(QRect(10, 20, 351, 251))
         self.addProjectMenuLayout = QGridLayout(self.widget)
@@ -57,27 +57,27 @@ class Ui_Form(object):
 
         self.returnExitLayout.addWidget(self.returnToMainAddProject)
 
-        self.addProjectExit = QPushButton(self.widget)
-        self.addProjectExit.setObjectName(u"addProjectExit")
+        self.exitAddProject = QPushButton(self.widget)
+        self.exitAddProject.setObjectName(u"exitAddProject")
 
-        self.returnExitLayout.addWidget(self.addProjectExit)
+        self.returnExitLayout.addWidget(self.exitAddProject)
 
 
         self.addProjectMenuLayout.addLayout(self.returnExitLayout, 1, 0, 1, 1)
 
 
-        self.retranslateUi(Form)
+        self.retranslateUi(addNewProject)
 
-        QMetaObject.connectSlotsByName(Form)
+        QMetaObject.connectSlotsByName(addNewProject)
     # setupUi
 
-    def retranslateUi(self, Form):
-        Form.setWindowTitle(QCoreApplication.translate("Form", u"Project Planner 1.0 - Add new project", None))
-        self.addEveryday.setText(QCoreApplication.translate("Form", u"Add everyday\n"
+    def retranslateUi(self, addNewProject):
+        addNewProject.setWindowTitle(QCoreApplication.translate("addNewProject", u"Project Planner 1.0 - Add new project", None))
+        self.addEveryday.setText(QCoreApplication.translate("addNewProject", u"Add everyday\n"
 "project", None))
-        self.addProgramming.setText(QCoreApplication.translate("Form", u"Add programming\n"
+        self.addProgramming.setText(QCoreApplication.translate("addNewProject", u"Add programming\n"
 "project", None))
-        self.returnToMainAddProject.setText(QCoreApplication.translate("Form", u"Return to main menu", None))
-        self.addProjectExit.setText(QCoreApplication.translate("Form", u"Exit program", None))
+        self.returnToMainAddProject.setText(QCoreApplication.translate("addNewProject", u"Return to main menu", None))
+        self.exitAddProject.setText(QCoreApplication.translate("addNewProject", u"Exit program", None))
     # retranslateUi
 
