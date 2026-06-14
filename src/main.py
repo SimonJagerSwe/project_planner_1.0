7874b691236bab0595f367bdc9991f27be7ee242
Simon Jäger <simonjager@hotmail.com>
2026-06-15 00:34:01 +0200

    opening project menu works

--- src/main.py
+++ src/main.py
@@ -23,20 +23,23 @@ from PySide6.QtWidgets import QApplication, QMainWindow, QStackedWidget
 
 def main():
     app = QApplication(sys.argv)
-    home_screen = QMainWindow()
+    # home_screen = QMainWindow()
     stack = QStackedWidget()
-    home_screen.setCentralWidget(stack)
-    main_window = load_ui("interface/main.ui")
-    main_page = main_window.centralWidget()
-    # main_page = (
-    #     main_window.centralWidget()
-    #     if hasattr(main_window, "centralWidget")
-    #     else main_window)
-    project_interface = load_ui("interface/newProject.ui")
-
+    # home_screen.setCentralWidget(stack)
+    home_screen = load_ui("interface/main.ui")
+    # main_page = main_window.centralWidget()
+    main_page = home_screen.takeCentralWidget()
+    
+    stack = QStackedWidget()
     stack.addWidget(main_page)
+    # stack.addWidget(main_page)
+    # stack.addWidget(project_interface)
+    # stack.setCurrentWidget(main_page)
+
+    project_interface = load_ui("interface/newProject.ui")
     stack.addWidget(project_interface)
     stack.setCurrentWidget(main_page)
+    home_screen.setCentralWidget(stack)
     button_handler.connect_buttons(main_page, stack, project_interface)
     home_screen.show()
     sys.exit(app.exec())
