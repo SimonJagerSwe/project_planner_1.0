11271f59d900cb9ea05c30f9eb380a2166db8916
Simon Jäger <simonjager@hotmail.com>
2026-06-24 22:08:38 +0200

    printing projects in string format works

--- src/project_printers.py
+++ src/project_printers.py
@@ -13,27 +13,27 @@ def print_projects(top_tab, sub_tab):
     print(f"Receiving top tab: {top_tab}\nBottom tab: {sub_tab}\n")
     project_file = resources.tab_handler[top_tab][sub_tab]
     projects = load_file(project_file)
-    # print(projects)
-    if sub_tab == 0:
+    if sub_tab == 3:
         for project in projects:
-            name = project["Project name"]
-            start = project["Project start date"]
-            end = project["Project end date"]
-            notes = project["Project notes"]
-            progress = project["Project progress"]
-            status = project["Project status"]
-            full_project = f"{name}\n{start}\n{end}\n{notes}\n{progress}\n{status}\n"
+            name = project["Task name"]
+            frequency = project["Task frequency"]
+            notes = project["Task notes"]
+            full_project = f"{name}\n{frequency}\n{notes}\n"
             print(full_project)
-
-    elif sub_tab == 1:
+    else:
         for project in projects:
             name = project["Project name"]
             start = project["Project start date"]
             end = project["Project end date"]
-            language = project["Language(s)"]
-            link = project["GitHub link"]
+            if "Language(s)" in project:
+                language = project["Language(s)"]
+            if "GitHub link" in project:
+                link = project["GitHub link"]
             notes = project["Project notes"]
             progress = project["Project progress"]
             status = project["Project status"]
-            full_project = f"{name}\n{start}\n{end}\n{language}\n{link}\n{notes}\n{progress}\n{status}\n"
+            if "Language(s)" in project:
+                full_project = f"{name}\n{start}\n{end}\n{language}\n{link}\n{notes}\n{progress}\n{status}\n"
+            else:
+                full_project = f"{name}\n{start}\n{end}\n{notes}\n{progress}\n{status}\n"
             print(full_project)
