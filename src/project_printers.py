2606ecd1239c657dd4ed4d5d539f3d6bb8d938a0
Simon Jäger <simonjager@hotmail.com>
2026-06-25 17:33:21 +0200

    writing to and reading from full project file works

--- src/project_printers.py
+++ src/project_printers.py
@@ -17,8 +17,28 @@ def print_projects(ui, top_tab, sub_tab):
     project_file = resources.tab_handler[top_tab][sub_tab]
     projects = load_file(project_file)
 
+    # Print all projects
+    if sub_tab == 2:
+        for project in projects:
+            name = project["Project name"]
+            start = project["Project start"]
+            end = project["Project end"]
+            if "Language(s)" in project:
+                language = project["Language(s)"]
+            if "GitHub link" in project:
+                link = project["GitHub link"]
+            notes = project["Project notes"]
+            progress = project["Project progress"]
+            status = project["Project status"]
+            if "Language(s)" in project:
+                full_project = f"Project name:\t{name}\nStart date:\t\t{start}\nEnd date:\t\t{end}\nLanguage(s):\t\t{language}\nGitHub link:\t\t{link}\nProject notes:\t{notes}\nProject progress:\t{progress}\nProject status:\t{status}\n"
+                ui.allProjects.addItem(full_project)
+            else:
+                full_project = f"Project name:\t{name}\nStart date:\t\t{start}\nEnd date:\t\t{end}\nProject notes:\t{notes}\nProject progress:\t{progress}\nProject status:\t{status}\n"
+                ui.allProjects.addItem(full_project)
+
     # Print precurring tasks depending on frequency
-    if sub_tab == 3:
+    elif sub_tab == 3:
         for project in projects:
             name = project["Task name"]
             frequency = project["Task frequency"]
