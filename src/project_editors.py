48572984ff1a6deffd9921d64d72b1c8bc55fb52
Simon Jäger <simonjager@hotmail.com>
2026-07-02 17:10:35 +0200

    parsing project files to find correct project

--- src/project_editors.py
+++ src/project_editors.py
@@ -5,10 +5,11 @@ import resources, writers
 from interface.ui_everyday import Ui_everydayProjectEditor
 from interface.ui_programming import Ui_programmingProjectEditor 
 from interface.ui_recurring import Ui_recurringProjectEditor
+from loader import load_file as loader
 
 from PySide6.QtWidgets import QDialog
 
-
+# Function to determine which ui to use for editing
 def edit_parser(project):
     print(f"Project to edit:\n{project.text()}")
     if "Language(s)" in project.text():
@@ -19,11 +20,40 @@ def edit_parser(project):
         edit_recurring(ui, project)
     else:
         ui = Ui_everydayProjectEditor()
-        edit_everyday(ui, project) 
+        edit_everyday(ui, project)
+
+
+# Get the correct projects file to delete old version of project
+def project_parser(project, type):
+    # Find project name
+    for var in project.text().split("\n"):
+        if "name" in var:
+            print(var)
+            project_name = var.split(":")[1].strip()
+            print(project_name)
+    
+    # Load everyday file
+    if type == "everyday":
+        e_projects = loader(resources.EVERYDAY_FILE)
+        for project in e_projects:
+            if project["Project name"] == project_name:
+                print(f"Project found:\n{project}")
+    elif type == "programming":
+        p_projects = loader(resources.PROGRAMING_FILE)
+        for project in p_projects:
+            if project["Project name"] == project_name:
+                print(f"Project found:\n{project}")
+        
+    else:
+        r_tasks = loader(resources.RECURRING_FILE)
+        for task in r_tasks:
+            if task["Task name"] == project_name:
+                print(f"Task found:\n{project_name}")
 
 
 def edit_everyday(ui, project):
     print("Editing everyday...")
+    project_parser(project, "everyday")
     dialog = QDialog()
     ui.setupUi(dialog)
     dialog.exec()
@@ -31,6 +61,7 @@ def edit_everyday(ui, project):
 
 def edit_programming(ui, project):
     print("Editing programming...")
+    project_parser(project, "programming")
     dialog = QDialog()
     ui.setupUi(dialog)
     dialog.exec()
@@ -38,6 +69,7 @@ def edit_programming(ui, project):
 
 def edit_recurring(ui, project):
     print("Editing recurring project...")
+    project_parser(project, "recurring")
     dialog = QDialog()
     ui.setupUi(dialog)
     dialog.exec()
