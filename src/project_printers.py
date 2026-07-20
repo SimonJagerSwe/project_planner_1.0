d4a5c38daa525c87a1d863bd41e6906cfdad46de
Simon Jäger <simonjager@hotmail.com>
2026-07-20 12:37:33 +0200

    start implementing archive printing

--- src/project_printers.py
+++ src/project_printers.py
@@ -18,60 +18,62 @@ def print_projects(ui, top_tab, sub_tab):
     project_file = resources.tab_handler[top_tab][sub_tab]
     projects = load_file(project_file)
 
+    # Print active projects
+    if top_tab == 0:
     # Print all projects
-    if sub_tab == 2:
-        for project in projects:
-            if "Project name" not in project:
-                continue
-            name = project["Project name"]
-            start = project["Project start"]
-            end = project["Project end"]
-            if "Language(s)" in project:
-                language = project["Language(s)"]
-            if "GitHub link" in project:
-                link = project["GitHub link"]
-            notes = project["Project notes"]
-            progress = project["Project progress"]
-            status = project["Project status"]
-            if "Language(s)" in project:
-                full_project = f"Project name:\t{name}\nStart date:\t\t{start}\nEnd date:\t\t{end}\nLanguage(s):\t\t{language}\nGitHub link:\t\t{link}\nProject notes:\t{notes}\nProject progress:\t{progress}\nProject status:\t{status}\n"
-                ui.allProjects.addItem(full_project)
-            else:
-                full_project = f"Project name:\t{name}\nStart date:\t\t{start}\nEnd date:\t\t{end}\nProject notes:\t{notes}\nProject progress:\t{progress}\nProject status:\t{status}\n"
-                ui.allProjects.addItem(full_project)
-    # Print precurring tasks depending on frequency
-    elif sub_tab == 3:
-        for project in projects:
-            name = project["Task name"]
-            frequency = project["Task frequency"]
-            notes = project["Task notes"]
-            full_project = f"Task name:\t\t{name}\nTask frequency:\t{frequency}\nTask notes:\t\t{notes}\n"
-            if frequency == "Weekly":
-                ui.recurringWeekly.addItem(full_project)
-            elif frequency == "Bi-weekly":
-                ui.recurringBi.addItem(full_project)
-            else:
-                ui.recurringOther.addItem(full_project)
-    # Print everyday or programming projects, based on whether programming-specific variables exist
-    else:
-        for project in projects:
-            if "Project name" not in project:
-                continue
-            name = project["Project name"]
-            start = project["Project start"]
-            end = project["Project end"]
-            if "Language(s)" in project:
-                language = project["Language(s)"]
-            if "GitHub link" in project:
-                link = project["GitHub link"]
-            notes = project["Project notes"]
-            progress = project["Project progress"]
-            status = project["Project status"]
+        if sub_tab == 2:
+            for project in projects:
+                if "Project name" not in project:
+                    continue
+                name = project["Project name"]
+                start = project["Project start"]
+                end = project["Project end"]
+                if "Language(s)" in project:
+                    language = project["Language(s)"]
+                if "GitHub link" in project:
+                    link = project["GitHub link"]
+                notes = project["Project notes"]
+                progress = project["Project progress"]
+                status = project["Project status"]
+                if "Language(s)" in project:
+                    full_project = f"Project name:\t{name}\nStart date:\t\t{start}\nEnd date:\t\t{end}\nLanguage(s):\t\t{language}\nGitHub link:\t\t{link}\nProject notes:\t{notes}\nProject progress:\t{progress}\nProject status:\t{status}\n"
+                    ui.allProjects.addItem(full_project)
+                else:
+                    full_project = f"Project name:\t{name}\nStart date:\t\t{start}\nEnd date:\t\t{end}\nProject notes:\t{notes}\nProject progress:\t{progress}\nProject status:\t{status}\n"
+                    ui.allProjects.addItem(full_project)
+        # Print precurring tasks depending on frequency
+        elif sub_tab == 3:
+            for project in projects:
+                name = project["Task name"]
+                frequency = project["Task frequency"]
+                notes = project["Task notes"]
+                full_project = f"Task name:\t\t{name}\nTask frequency:\t{frequency}\nTask notes:\t\t{notes}\n"
+                if frequency == "Weekly":
+                    ui.recurringWeekly.addItem(full_project)
+                elif frequency == "Bi-weekly":
+                    ui.recurringBi.addItem(full_project)
+                else:
+                    ui.recurringOther.addItem(full_project)
+        # Print everyday or programming projects, based on whether programming-specific variables exist
+        else:
+            for project in projects:
+                if "Project name" not in project:
+                    continue
+                name = project["Project name"]
+                start = project["Project start"]
+                end = project["Project end"]
+                if "Language(s)" in project:
+                    language = project["Language(s)"]
+                if "GitHub link" in project:
+                    link = project["GitHub link"]
+                notes = project["Project notes"]
+                progress = project["Project progress"]
+                status = project["Project status"]
 
-            if "Language(s)" in project:
-                full_project = f"Project name:\t{name}\nStart date:\t\t{start}\nEnd date:\t\t{end}\nLanguage(s):\t\t{language}\nGitHub link:\t\t{link}\nProject notes:\t{notes}\nProject progress:\t{progress}\nProject status:\t{status}\n"
-                ui.programmingProjects.addItem(full_project)
-            else:
-                full_project = f"Project name:\t{name}\nStart date:\t\t{start}\nEnd date:\t\t{end}\nProject notes:\t{notes}\nProject progress:\t{progress}\nProject status:\t{status}\n"
-                ui.everydayProjects.addItem(full_project)
+                if "Language(s)" in project:
+                    full_project = f"Project name:\t{name}\nStart date:\t\t{start}\nEnd date:\t\t{end}\nLanguage(s):\t\t{language}\nGitHub link:\t\t{link}\nProject notes:\t{notes}\nProject progress:\t{progress}\nProject status:\t{status}\n"
+                    ui.programmingProjects.addItem(full_project)
+                else:
+                    full_project = f"Project name:\t{name}\nStart date:\t\t{start}\nEnd date:\t\t{end}\nProject notes:\t{notes}\nProject progress:\t{progress}\nProject status:\t{status}\n"
+                    ui.everydayProjects.addItem(full_project)
             
