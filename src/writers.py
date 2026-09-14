6c14f6bedc2e3b06345e58fd53c2915625fee2e1
Simon Jäger <simonjager@hotmail.com>
2026-09-14 22:09:48 +0200

    small fix

--- src/writers.py
+++ src/writers.py
@@ -74,6 +74,7 @@ def writer(project, project_type, current_dialog, main_window, write_type, retur
     print(f"Writing file using:\n{project}\n")
     print(f"Project type to write:\n{project_type}\n")
     print(f"Write type:\n{write_type}\n")
+    
     # This is for resetting recurring task statuses
     if write_type == "reset all":
         print("Resetting all recurring tasks of current type\n")
